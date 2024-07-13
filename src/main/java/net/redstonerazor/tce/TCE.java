05ef3f3c4caa8f5951b9bede68c4a26a02bfc7ab
TheRedstoneRazor <redstonerazor@hotmail.com>
2024-07-13 19:06:59 +0100

    ouchie my hands

--- src/main/java/net/redstonerazor/tce/TCE.java
+++ src/main/java/net/redstonerazor/tce/TCE.java
@@ -2,6 +2,7 @@ package net.redstonerazor.tce;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.redstonerazor.tce.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,5 +20,6 @@ public class TCE implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
